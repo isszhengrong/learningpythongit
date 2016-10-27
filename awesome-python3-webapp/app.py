b838c0449a0d7913b864a9c652925ce636c79327
root <root@mango.zrr>
2016-10-27 02:32:50 -0400

    a little modifidation

--- awesome-python3-webapp/app.py
+++ awesome-python3-webapp/app.py
@@ -1,4 +1,4 @@
-import logging:logging.basicConfig(level=logging.INFO)
+import logging;logging.basicConfig(level=logging.INFO)
 import asyncio,os,json,time
 from datetime import datetime
 from aiohttp import web
@@ -9,7 +9,7 @@ def index(request):
 def init(loop):
     app=web.Application(loop=loop)
     app.router.add_route('GET','/',index)
-    srv=yield from loop.create_server(app.make_handler(),'127.0.0.1',90000)
+    srv=yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
     logging.info('server started at http://127.0.0.1:9000')
     return srv
 
